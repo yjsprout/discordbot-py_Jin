3c224058cea57168ebb14dec9d2f44213cf2b08b
SWUF0rc3 <114800675+SWUF0rc3@users.noreply.github.com>
2023-02-11 14:55:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,29 +4,67 @@ import discord
 from dotenv import load_dotenv
 import os
 load_dotenv()
+from discord import app_commands
+from discord.ext import commands
+from datetime import datetime
+import sqlite3
 
 PREFIX = os.environ['PREFIX']
 TOKEN = os.environ['TOKEN']
 
-client = discord.Client()
+bot = commands.Bot(command_prefix="!", intents = discord.Intents.default())
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Logged in as {client.user}.')
+    print("봇 실행됨")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e :
+        print(e)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.tree.command(name="출석체크")
+async def check(interaction: discord.Interaction):
+    date_time = datetime.today().strftime('%Y-%m-%d %H:%M')
+    await interaction.response.send_message(f"{interaction.user.display_name} 출석했습니다.\n{date_time}")
+    # user.name -> 실제 사용자 이름
+    # user.display_name -> 서버에서 설정한 별명
 
-    if message.content == f'{PREFIX}call':
-        await message.channel.send("callback!")
+    conn = sqlite3.connect('Attendance.db')
+    cur = conn.cursor()
+    sql1 = "CREATE TABLE IF NOT EXISTS attTBL(name text,date_time text);"
+    sql2 = "INSERT INTO attTBL(name,date_time) values (?,?);"
+    cur.execute(sql1)
+    cur.execute(sql2, (interaction.user.display_name, date_time))
+    conn.commit()
+    cur.close()
 
-    if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello!')
+@bot.tree.command(name="say")
+@app_commands.describe(thing_to_say = "What should I say?")
+async def say(interaction: discord.Interaction, thing_to_say: str):
+    await interaction.response.send_message(f"{interaction.user.name} said: '{thing_to_say}'")
 
+@bot.tree.command(name="db조회")
+async def db(interaction: discord.Interaction):
+    conn = sqlite3.connect('Attendance.db')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM attTBL')
+    lrow=[]
+    for row in cur:
+        lrow.append(list(row))
+    await interaction.response.send_message(f"{lrow}")
+    cur.close()
+
+@bot.tree.command(name="resetdb")
+async def reset(interaction:discord.Interaction):
+    conn = sqlite3.connect('Attendance.db')
+    cur = conn.cursor()
+    sql3 = "DROP TABLE IF EXISTS attTBL"
+    cur.execute(sql3)
+    cur.close()
+    await interaction.response.send_message(f"초기화")
 
 try:
-    client.run(TOKEN)
+    bot.run(TOKEN)
 except discord.errors.LoginFailure as e:
     print("Improper token has been passed.")
