e5cc0518e5121995b3026d7508d89965481b2843
SWUF0rc3 <114800675+SWUF0rc3@users.noreply.github.com>
2023-02-27 15:08:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,7 +11,7 @@ load_dotenv()
 PREFIX = os.environ['PREFIX']
 TOKEN = os.environ['TOKEN']
 
-bot = commands.Bot(command_prefix="!", intents = discord.Intents.default())
+bot = commands.Bot(command_prefix="!")
 
 @bot.event
 async def on_ready():
