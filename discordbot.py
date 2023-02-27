5961ffc55906ecfba3781b1b8bb573c04be5bfba
SWUF0rc3 <114800675+SWUF0rc3@users.noreply.github.com>
2023-02-27 12:11:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,6 @@
 from cmath import log
 from distutils.sysconfig import PREFIX
 import discord
-from discord import app_commands
 from discord.ext import commands
 from datetime import datetime
 import sqlite3
