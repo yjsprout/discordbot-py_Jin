a4480f1f8fa6d9059d33f099c0444802b524379a
SWUF0rc3 <114800675+SWUF0rc3@users.noreply.github.com>
2023-02-27 12:21:58 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,18 +11,17 @@ load_dotenv()
 PREFIX = os.environ['PREFIX']
 TOKEN = os.environ['TOKEN']
 
-bot = commands.Bot(command_prefix="!", intents = discord.Intents.default())
 
 @bot.event
 async def on_ready():
     print("봇 실행됨")
     try:
-        synced = await bot.tree.sync()
+        synced = await bot.sync()
         print(f"Synced {len(synced)} command(s)")
     except Exception as e :
         print(e)
 
-@bot.tree.command(name="출석체크")
+@bot.command(name="출석체크")
 async def check(interaction: discord.Interaction):
     date_time = datetime.today().strftime('%Y-%m-%d %H:%M')
     await interaction.response.send_message(f"{interaction.user.display_name} 출석했습니다.\n{date_time}")
@@ -38,7 +37,7 @@ async def check(interaction: discord.Interaction):
     conn.commit()
     cur.close()
 
-@bot.tree.command(name="db조회")
+@bot.command(name="db조회")
 async def db(interaction: discord.Interaction):
     conn = sqlite3.connect('Attendance.db')
     cur = conn.cursor()
@@ -49,7 +48,7 @@ async def db(interaction: discord.Interaction):
     await interaction.response.send_message(f"{lrow}")
     cur.close()
 
-@bot.tree.command(name="resetdb")
+@bot.command(name="resetdb")
 async def reset(interaction:discord.Interaction):
     conn = sqlite3.connect('Attendance.db')
     cur = conn.cursor()
@@ -58,7 +57,7 @@ async def reset(interaction:discord.Interaction):
     cur.close()
     await interaction.response.send_message(f"데이터베이스 초기화를 완료하였습니다.")
 
-@bot.tree.command(name="absentees")
+@bot.command(name="absentees")
 async def checkAbs(interaction:discord.Interaction):
     conn = sqlite3.connect('Attendance.db')
     cur = conn.cursor()
